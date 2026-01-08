670815fb470b8a3617b2946d070c80dc5e943f8a
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2026-01-08 10:08:31 +0700

    add

---
+++ src/JavaCoBan/Welcome.java
@@ -0,0 +1,10 @@
+package JavaCoBan;
+
+import java.util.Date;
+
+public class Welcome {
+    public static void main(String[] args) {
+        Date now =  new Date();
+        System.out.println(now);
+    }
+}
