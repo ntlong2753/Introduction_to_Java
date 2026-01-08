690e6bf5ee580ae415953eac16529e15d9e374f9
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2026-01-08 14:12:58 +0700

    add

---
+++ src/BaiTap/HienThiLoiChao.java
@@ -0,0 +1,14 @@
+package BaiTap;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
